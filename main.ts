8f5c7a69a04fd3a19ac2b7e4eca771b09b9ae246
luissydseter <121295380+luissydseter@users.noreply.github.com>
2025-06-30 19:42:10 +0200

    update

--- main.ts
+++ main.ts
@@ -1,19 +1,13 @@
 scene.onHitWall(SpriteKind.Player, function (sprite, location) {
     mySprite.setVelocity(100, 0)
 })
-scene.onOverlapTile(SpriteKind.Player, img`end`, function (sprite, location) {
-    tiles.setCurrentTilemap(tilemap`level2`)
-    tiles.placeOnRandomTile(mySprite, img`start`)
-    scene.cameraFollowSprite(mySprite)
-    controller.moveSprite(mySprite)
-})
-scene.onOverlapTile(SpriteKind.Player, img`myTile`, function (sprite, location) {
+scene.onOverlapTile(SpriteKind.Player, assets.tile`transparency16`, function (sprite, location) {
     tiles.setCurrentTilemap(tilemap`level3`)
     tiles.placeOnTile(sprites.create(assets.image`butterfly`, SpriteKind.Enemy), tiles.getTileLocation(0, 15))
 })
 let mySprite: Sprite = null
 tiles.setCurrentTilemap(tilemap`level1`)
 mySprite = sprites.create(assets.image`mysprite`, SpriteKind.Player)
-tiles.placeOnRandomTile(mySprite, img`start`)
+tiles.placeOnRandomTile(mySprite, assets.tile`transparency16`)
 scene.cameraFollowSprite(mySprite)
 controller.moveSprite(mySprite)
